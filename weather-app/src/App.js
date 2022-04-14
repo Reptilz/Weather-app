669d5d008eb751b05758027254ea8303ba6ef857
Morlet <34801852+Reptilz@users.noreply.github.com>
2022-04-14 14:25:22 +0200

    installation sass + création du composant Search

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
-      <h1>World</h1>
+      <h1 className="title-page">Weather App</h1>
+      <Search />
     </div>
   );
 }
---
+++ weather-app/src/components/Search.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <div className="search-container">
+      <input type="text" className="search-bar" placeholder="Search..." />
+    </div>
+  );
+}
