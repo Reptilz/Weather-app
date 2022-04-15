f64e61fa3e2b0233403a007bff9864cd7a97e9e6
Morlet <34801852+Reptilz@users.noreply.github.com>
2022-04-15 12:18:42 +0200

    ajout du temps dans le component Weather

--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -4,7 +4,7 @@ export default function Weather({ weather }) {
   return (
     <div className="weather-container">
       <div className="temp">{Math.round(weather.main.temp)}°c</div>
-      <div className="weather">Sunny</div>
+      <div className="weather">{weather.weather[0].main}</div>
     </div>
   );
 }
